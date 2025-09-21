24f3589460d142e327da1ce5ece2702cf4fc2571
TheEnderCore <theendercore@gmail.com>
2025-09-21 17:31:01 +0300

    OcelotHeadModel

--- src/client/kotlin/org/teamvoided/all_the_heads/client/model/OcelotHeadModel.kt
+++ src/client/kotlin/org/teamvoided/all_the_heads/client/model/OcelotHeadModel.kt
@@ -33,11 +33,11 @@ class OcelotHeadModel(modelPart: ModelPart) : HeadModelBase() {
             mesh.root.addOrReplaceChild(
                 "head",
                 CubeListBuilder.create()
-                    .addBox("main", -2.5f, -2.0f, -3.0f, 5.0f, 4.0f, 5.0f, def)
-                    .addBox("nose", -1.5f, -0.001f, -4.0f, 3, 2, 2, def, 0, 24)
-                    .addBox("ear1", -2.0f, -3.0f, 0.0f, 1, 1, 2, def, 0, 10)
-                    .addBox("ear2", 1.0f, -3.0f, 0.0f, 1, 1, 2, def, 6, 10),
-                PartPose.offset(0.0f, -2.0f, 0.0f)
+                    .addBox("main", -2.5f, -4f, -2.5f, 5f, 4f, 5f, def)
+                    .addBox("nose", -1.5f, -2.001f, -3.5f, 3, 2, 2, def, 0, 24)
+                    .addBox("ear1", -2f, -5f, 0.5f, 1, 1, 2, def, 0, 10)
+                    .addBox("ear2", 1f, -5f, 0.5f, 1, 1, 2, def, 6, 10),
+                PartPose.ZERO
             )
             return LayerDefinition.create(mesh, 64, 32)
         }
