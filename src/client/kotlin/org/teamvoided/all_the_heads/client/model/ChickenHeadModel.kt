de249e7fde052477966570f19f1ac534c01fd2b7
TheEnderCore <theendercore@gmail.com>
2025-09-21 17:58:21 +0300

    ChickenHeadModel

--- src/client/kotlin/org/teamvoided/all_the_heads/client/model/ChickenHeadModel.kt
+++ src/client/kotlin/org/teamvoided/all_the_heads/client/model/ChickenHeadModel.kt
@@ -14,23 +14,14 @@ import org.teamvoided.all_the_heads.client.model.utils.HeadModelBase
 class ChickenHeadModel(modelPart: ModelPart) : HeadModelBase() {
     override fun getId(): ResourceLocation = ID
     private val head: ModelPart = modelPart.getChild("head")
-    private val beak = modelPart.getChild("beak")
-    private val redThing = modelPart.getChild("red_thing")
 
     override fun setupAnim(f: Float, g: Float, h: Float) {
         head.yRot = g * (Math.PI / 180.0).toFloat()
         head.xRot = h * (Math.PI / 180.0).toFloat()
-
-        beak.xRot = this.head.xRot
-        beak.yRot = this.head.yRot
-        redThing.xRot = this.head.xRot
-        redThing.yRot = this.head.yRot
     }
 
     override fun renderToBuffer(poseStack: PoseStack, vertexConsumer: VertexConsumer, i: Int, j: Int, k: Int) {
         head.render(poseStack, vertexConsumer, i, j, k)
-        beak.render(poseStack, vertexConsumer, i, j, k)
-        redThing.render(poseStack, vertexConsumer, i, j, k)
     }
 
     companion object {
@@ -38,19 +29,19 @@ class ChickenHeadModel(modelPart: ModelPart) : HeadModelBase() {
         fun head(): LayerDefinition {
             val mesh = MeshDefinition()
             val root = mesh.root
-            root.addOrReplaceChild(
+            val head = root.addOrReplaceChild(
                 "head",
-                CubeListBuilder.create().texOffs(0, 0).addBox(-2.0f, -6.0f, -2.0f, 4.0f, 6.0f, 3.0f),
+                CubeListBuilder.create().texOffs(0, 0).addBox(-2f, -6f, -1.5f, 4f, 6f, 3f),
                 PartPose.ZERO
             )
-            root.addOrReplaceChild(
+            head.addOrReplaceChild(
                 "beak",
-                CubeListBuilder.create().texOffs(14, 0).addBox(-2.0f, -4.0f, -4.0f, 4.0f, 2.0f, 2.0f),
+                CubeListBuilder.create().texOffs(14, 0).addBox(-2f, -4f, -3.5f, 4f, 2f, 2f),
                 PartPose.ZERO
             )
-            root.addOrReplaceChild(
+            head.addOrReplaceChild(
                 "red_thing",
-                CubeListBuilder.create().texOffs(14, 4).addBox(-1.0f, -2.0f, -3.0f, 2.0f, 2.0f, 2.0f),
+                CubeListBuilder.create().texOffs(14, 4).addBox(-1f, -2f, -2.5f, 2f, 2f, 2f),
                 PartPose.ZERO
             )
             return LayerDefinition.create(mesh, 64, 32)
