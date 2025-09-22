0d70bfdd463537384118cefa2951ebf6407f0b51
TheEnderCore <theendercore@gmail.com>
2025-09-22 15:07:33 +0300

    FrogHeadModel

--- src/client/kotlin/org/teamvoided/all_the_heads/client/model/FrogHeadModel.kt
+++ src/client/kotlin/org/teamvoided/all_the_heads/client/model/FrogHeadModel.kt
@@ -4,9 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.blaze3d.vertex.VertexConsumer
 import net.minecraft.client.model.geom.ModelPart
 import net.minecraft.client.model.geom.PartPose
-import net.minecraft.client.model.geom.builders.CubeListBuilder
-import net.minecraft.client.model.geom.builders.LayerDefinition
-import net.minecraft.client.model.geom.builders.MeshDefinition
+import net.minecraft.client.model.geom.builders.*
 import net.minecraft.resources.ResourceLocation
 import org.teamvoided.all_the_heads.AllTheHeads
 import org.teamvoided.all_the_heads.client.model.utils.HeadModelBase
@@ -14,10 +12,19 @@ import org.teamvoided.all_the_heads.client.model.utils.HeadModelBase
 class FrogHeadModel(modelPart: ModelPart) : HeadModelBase() {
     override fun getId(): ResourceLocation = ID
     private val head: ModelPart = modelPart.getChild("head")
+    private val croakingBody: ModelPart = head.getChild("croaking_body")
 
     override fun setupAnim(f: Float, g: Float, h: Float) {
         head.yRot = g * (Math.PI / 180.0).toFloat()
         head.xRot = h * (Math.PI / 180.0).toFloat()
+        croakingBody.visible = f != 0f
+        /*  if (croakingBody.visible) {
+              val scale = sin(f * 0.1f) + 1f
+              Minecraft.getInstance().player?.displayClientMessage(Component.literal("Scale: $scale"), true)
+              croakingBody.xScale = scale
+              croakingBody.yScale = scale * 1.2f
+              croakingBody.zScale = scale
+          }*/
     }
 
     override fun renderToBuffer(poseStack: PoseStack, vertexConsumer: VertexConsumer, i: Int, j: Int, k: Int) {
@@ -28,23 +35,39 @@ class FrogHeadModel(modelPart: ModelPart) : HeadModelBase() {
         val ID = AllTheHeads.id("frog")
         fun head(): LayerDefinition {
             val mesh = MeshDefinition()
-            val head = mesh.root.addOrReplaceChild(
+
+            val head: PartDefinition = mesh.root.addOrReplaceChild(
                 "head",
-                CubeListBuilder.create().texOffs(23, 13).addBox(-3.5f, -1.0f, -7.0f, 7.0f, 0.0f, 9.0f).texOffs(0, 13)
-                    .addBox(-3.5f, -2.0f, -7.0f, 7.0f, 3.0f, 9.0f),
-                PartPose.offset(0.0f, -2.0f, -1.0f)
+                CubeListBuilder.create()
+                    .texOffs(3, 1).addBox(-3.5f, -3f, -4.5f, 7f, 3f, 9f)
+                    .texOffs(23, 22).addBox(-3.5f, -1f, -4.5f, 7f, 0f, 9f),
+                PartPose.ZERO
             )
-            val partDefinition5 =
-                head.addOrReplaceChild("eyes", CubeListBuilder.create(), PartPose.offset(-0.5f, 0.0f, 2.0f))
-            partDefinition5.addOrReplaceChild(
+            head.addOrReplaceChild(
+                "croaking_body",
+                CubeListBuilder.create().texOffs(26, 5)
+                    .addBox(-3.5f, -2.0f, -4.5f, 7f, 2f, 3f, CubeDeformation(-0.1f)),
+                PartPose.ZERO
+            )
+
+
+            val trueHead = head.addOrReplaceChild(
+                "true_head",
+                CubeListBuilder.create().texOffs(23, 13).addBox(-3.5f, -1f, -4.5f, 7f, 0f, 9f).texOffs(0, 13)
+                    .addBox(-3.5f, -3f, -4.5f, 7f, 3f, 9f),
+                PartPose.offset(0f, -2f, 0f)
+            )
+            val eyes =
+                trueHead.addOrReplaceChild("eyes", CubeListBuilder.create(), PartPose.offset(0f, -3f, -2f))
+            eyes.addOrReplaceChild(
                 "right_eye",
-                CubeListBuilder.create().texOffs(0, 0).addBox(-1.5f, -1.0f, -1.5f, 3.0f, 2.0f, 3.0f),
-                PartPose.offset(-1.5f, -3.0f, -6.5f)
+                CubeListBuilder.create().texOffs(0, 0).addBox(-1.5f, -2f, -1.5f, 3f, 2f, 3f),
+                PartPose.offset(-2f, 0f, 0f)
             )
-            partDefinition5.addOrReplaceChild(
+            eyes.addOrReplaceChild(
                 "left_eye",
-                CubeListBuilder.create().texOffs(0, 5).addBox(-1.5f, -1.0f, -1.5f, 3.0f, 2.0f, 3.0f),
-                PartPose.offset(2.5f, -3.0f, -6.5f)
+                CubeListBuilder.create().texOffs(0, 5).addBox(-1.5f, -2f, -1.5f, 3f, 2f, 3f),
+                PartPose.offset(2f, 0f, 0f)
             )
             return LayerDefinition.create(mesh, 48, 48)
         }
