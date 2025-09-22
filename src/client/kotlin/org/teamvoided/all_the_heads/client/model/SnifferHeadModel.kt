c5a87d96cb9c217e522b5ff0828e89c7ff5ec00f
TheEnderCore <theendercore@gmail.com>
2025-09-22 16:18:32 +0300

    SnifferHeadModel

--- src/client/kotlin/org/teamvoided/all_the_heads/client/model/SnifferHeadModel.kt
+++ src/client/kotlin/org/teamvoided/all_the_heads/client/model/SnifferHeadModel.kt
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.blaze3d.vertex.VertexConsumer
 import net.minecraft.client.model.geom.ModelPart
 import net.minecraft.client.model.geom.PartPose
-import net.minecraft.client.model.geom.builders.CubeDeformation
 import net.minecraft.client.model.geom.builders.CubeListBuilder
 import net.minecraft.client.model.geom.builders.LayerDefinition
 import net.minecraft.client.model.geom.builders.MeshDefinition
@@ -32,35 +31,29 @@ class SnifferHeadModel(modelPart: ModelPart) : HeadModelBase() {
             val head = mesh.root.addOrReplaceChild(
                 "head",
                 CubeListBuilder.create()
-                    .texOffs(8, 15)
-                    .addBox(-6.5f, -7.5f, -11.5f, 13.0f, 18.0f, 11.0f, CubeDeformation(0.0f))
-                    .texOffs(8, 4)
-                    .addBox(-6.5f, 7.5f, -11.5f, 13.0f, 0.0f, 11.0f, CubeDeformation(0.0f)),
-                PartPose.offset(0f, -7.5f, 0f)
+                    .texOffs(8, 15).addBox(-6.5f, -15f, -5.5f, 13f, 18f, 11f)
+                    .texOffs(8, 4).addBox(-6.5f, 0f, -5.5f, 13f, 0f, 11f),
+                PartPose.ZERO
             )
             head.addOrReplaceChild(
                 "left_ear",
-                CubeListBuilder.create().texOffs(2, 0)
-                    .addBox(0.0f, 0.0f, -3.0f, 1.0f, 19.0f, 7.0f, CubeDeformation(0.0f)),
-                PartPose.offset(6.51f, -7.5f, -4.51f)
+                CubeListBuilder.create().texOffs(2, 0).addBox(6.5f, -15f, -1.5f, 1f, 19f, 7f),
+                PartPose.ZERO
             )
             head.addOrReplaceChild(
                 "right_ear",
-                CubeListBuilder.create().texOffs(48, 0)
-                    .addBox(-1.0f, 0.0f, -3.0f, 1.0f, 19.0f, 7.0f, CubeDeformation(0.0f)),
-                PartPose.offset(-6.51f, -7.5f, -4.51f)
+                CubeListBuilder.create().texOffs(48, 0).addBox(-7.5f, -15f, -1.5f, 1f, 19f, 7f),
+                PartPose.ZERO
             )
             head.addOrReplaceChild(
                 "nose",
-                CubeListBuilder.create().texOffs(10, 45)
-                    .addBox(-6.5f, -2.0f, -9.0f, 13.0f, 2.0f, 9.0f, CubeDeformation(0.0f)),
-                PartPose.offset(0.0f, -4.5f, -11.5f)
+                CubeListBuilder.create().texOffs(10, 45).addBox(-6.5f, -14f, -14.5f, 13f, 2f, 9f),
+                PartPose.ZERO
             )
             head.addOrReplaceChild(
                 "lower_beak",
-                CubeListBuilder.create().texOffs(10, 57)
-                    .addBox(-6.5f, -7.0f, -8.0f, 13.0f, 12.0f, 9.0f, CubeDeformation(0.0f)),
-                PartPose.offset(0.0f, 2.5f, -12.5f)
+                CubeListBuilder.create().texOffs(10, 57).addBox(-6.5f, -12f, -14.5f, 13f, 12f, 9f),
+                PartPose.ZERO
             )
             return LayerDefinition.create(mesh, 192, 192)
         }
