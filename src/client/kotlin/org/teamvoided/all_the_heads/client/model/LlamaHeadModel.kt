56b70be01853ee06a23e6611767bba89e28a9a7d
TheEnderCore <theendercore@gmail.com>
2025-09-22 17:04:38 +0300

    LlamaHeadModel

--- src/client/kotlin/org/teamvoided/all_the_heads/client/model/LlamaHeadModel.kt
+++ src/client/kotlin/org/teamvoided/all_the_heads/client/model/LlamaHeadModel.kt
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.blaze3d.vertex.VertexConsumer
 import net.minecraft.client.model.geom.ModelPart
 import net.minecraft.client.model.geom.PartPose
-import net.minecraft.client.model.geom.builders.CubeDeformation
 import net.minecraft.client.model.geom.builders.CubeListBuilder
 import net.minecraft.client.model.geom.builders.LayerDefinition
 import net.minecraft.client.model.geom.builders.MeshDefinition
