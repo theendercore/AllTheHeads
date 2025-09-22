a1102ce9ddda80caa0b355d6983d23f6241ed1ab
TheEnderCore <theendercore@gmail.com>
2025-09-22 16:39:28 +0300

    ArmadilloHeadModel

--- src/client/kotlin/org/teamvoided/all_the_heads/client/model/ArmadilloHeadModel.kt
+++ src/client/kotlin/org/teamvoided/all_the_heads/client/model/ArmadilloHeadModel.kt
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.blaze3d.vertex.VertexConsumer
 import net.minecraft.client.model.geom.ModelPart
 import net.minecraft.client.model.geom.PartPose
-import net.minecraft.client.model.geom.builders.CubeDeformation
 import net.minecraft.client.model.geom.builders.CubeListBuilder
 import net.minecraft.client.model.geom.builders.LayerDefinition
 import net.minecraft.client.model.geom.builders.MeshDefinition
@@ -33,35 +32,32 @@ class ArmadilloHeadModel(modelPart: ModelPart) : HeadModelBase() {
             val head = mesh.root.addOrReplaceChild(
                 "head",
                 CubeListBuilder.create(),
-                PartPose.offset(0.0f, -4.0f, 0.0f)
+                PartPose.offset(0f, -4f, 0f)
             )
             head.addOrReplaceChild(
                 "head_cube",
-                CubeListBuilder.create().texOffs(43, 15)
-                    .addBox(-1.5f, -1.0f, -1.0f, 3.0f, 5.0f, 2.0f, CubeDeformation(0.0f)),
-                PartPose.offsetAndRotation(0.0f, 0.0f, 0.0f, -0.3927f, 0.0f, 0.0f)
+                CubeListBuilder.create().texOffs(43, 15).addBox(-1.5f, -1f, -1f, 3f, 5f, 2f),
+                PartPose.offsetAndRotation(0f, 0f, 0f, -0.3927f, 0f, 0f)
             )
             val rightEar = head.addOrReplaceChild(
                 "right_ear",
                 CubeListBuilder.create(),
-                PartPose.offset(-1.0f, -1.0f, 0.0f)
+                PartPose.offset(-1f, -1f, 0f)
             )
             rightEar.addOrReplaceChild(
                 "right_ear_cube",
-                CubeListBuilder.create().texOffs(43, 10)
-                    .addBox(-2.0f, -3.0f, 0.0f, 2.0f, 5.0f, 0.0f, CubeDeformation(0.0f)),
-                PartPose.offsetAndRotation(-0.5f, 0.0f, -0.6f, 0.1886f, -0.3864f, -0.0718f)
+                CubeListBuilder.create().texOffs(43, 10).addBox(-2f, -3f, 0f, 2f, 5f, 0f),
+                PartPose.offsetAndRotation(-0.5f, 0f, -0.6f, 0.1886f, -0.3864f, -0.0718f)
             )
             val leftEar = head.addOrReplaceChild(
                 "left_ear",
                 CubeListBuilder.create(),
-                PartPose.offset(1.0f, -2.0f, 0.0f)
+                PartPose.offset(1f, -2f, 0f)
             )
             leftEar.addOrReplaceChild(
                 "left_ear_cube",
-                CubeListBuilder.create().texOffs(47, 10)
-                    .addBox(0.0f, -3.0f, 0.0f, 2.0f, 5.0f, 0.0f, CubeDeformation(0.0f)),
-                PartPose.offsetAndRotation(0.5f, 1.0f, -0.6f, 0.1886f, 0.3864f, 0.0718f)
+                CubeListBuilder.create().texOffs(47, 10).addBox(0f, -3f, 0f, 2f, 5f, 0f),
+                PartPose.offsetAndRotation(0.5f, 1f, -0.6f, 0.1886f, 0.3864f, 0.0718f)
             )
 
             return LayerDefinition.create(mesh, 64, 64)
