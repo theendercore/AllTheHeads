9dadfdb28f443fd3901ec23a1ed845915d9461f5
TheEnderCore <theendercore@gmail.com>
2025-09-24 15:31:43 +0300

    HoglinHeadModel

--- src/client/kotlin/org/teamvoided/all_the_heads/client/model/HoglinHeadModel.kt
+++ src/client/kotlin/org/teamvoided/all_the_heads/client/model/HoglinHeadModel.kt
@@ -4,7 +4,9 @@ import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.blaze3d.vertex.VertexConsumer
 import net.minecraft.client.model.geom.ModelPart
 import net.minecraft.client.model.geom.PartPose
-import net.minecraft.client.model.geom.builders.*
+import net.minecraft.client.model.geom.builders.CubeListBuilder
+import net.minecraft.client.model.geom.builders.LayerDefinition
+import net.minecraft.client.model.geom.builders.MeshDefinition
 import net.minecraft.resources.ResourceLocation
 import org.teamvoided.all_the_heads.AllTheHeads
 import org.teamvoided.all_the_heads.client.model.utils.HeadModelBase
@@ -26,30 +28,30 @@ class HoglinHeadModel(modelPart: ModelPart) : HeadModelBase() {
         val ID = AllTheHeads.id("hoglin")
         fun head(): LayerDefinition {
             val mesh = MeshDefinition()
-            val head  = mesh.root.addOrReplaceChild(
+            val head = mesh.root.addOrReplaceChild(
                 "head",
-                CubeListBuilder.create().texOffs(61, 1).addBox(-7.0f, -3.0f, -19.0f, 14.0f, 6.0f, 19.0f),
-                PartPose.offset(0.0f, -3f, 0.0f)
+                CubeListBuilder.create().texOffs(61, 1).addBox(-7f, -3f, -9.5f, 14f, 6f, 19f),
+                PartPose.offset(0f, -3f, 0f)
             )
             head.addOrReplaceChild(
                 "right_ear",
-                CubeListBuilder.create().texOffs(1, 1).addBox(-6.0f, -1.0f, -2.0f, 6.0f, 1.0f, 4.0f),
-                PartPose.offsetAndRotation(-6.0f, -2.0f, -3.0f, 0.0f, 0.0f, (-Math.PI * 2.0 / 9.0).toFloat())
+                CubeListBuilder.create().texOffs(1, 1).addBox(-6f, -1f, 7.5f, 6f, 1f, 4f),
+                PartPose.offsetAndRotation(-6f, -2f, -3f, 0f, 0f, (-Math.PI * 2.0 / 9.0).toFloat())
             )
             head.addOrReplaceChild(
                 "left_ear",
-                CubeListBuilder.create().texOffs(1, 6).addBox(0.0f, -1.0f, -2.0f, 6.0f, 1.0f, 4.0f),
-                PartPose.offsetAndRotation(6.0f, -2.0f, -3.0f, 0.0f, 0.0f, (Math.PI * 2.0 / 9.0).toFloat())
+                CubeListBuilder.create().texOffs(1, 6).addBox(0f, -1f, 7.5f, 6f, 1f, 4f),
+                PartPose.offsetAndRotation(6f, -2f, -3f, 0f, 0f, (Math.PI * 2.0 / 9.0).toFloat())
             )
             head.addOrReplaceChild(
                 "right_horn",
-                CubeListBuilder.create().texOffs(10, 13).addBox(-1.0f, -11.0f, -1.0f, 2.0f, 11.0f, 2.0f),
-                PartPose.offset(-7.0f, 2.0f, -12.0f)
+                CubeListBuilder.create().texOffs(10, 13).addBox(-1f, -11f, 8.5f, 2f, 11f, 2f),
+                PartPose.offset(-7f, 2f, -12f)
             )
             head.addOrReplaceChild(
                 "left_horn",
-                CubeListBuilder.create().texOffs(1, 13).addBox(-1.0f, -11.0f, -1.0f, 2.0f, 11.0f, 2.0f),
-                PartPose.offset(7.0f, 2.0f, -12.0f)
+                CubeListBuilder.create().texOffs(1, 13).addBox(-1f, -11f, 8.5f, 2f, 11f, 2f),
+                PartPose.offset(7f, 2f, -12f)
             )
             return LayerDefinition.create(mesh, 128, 64)
         }
