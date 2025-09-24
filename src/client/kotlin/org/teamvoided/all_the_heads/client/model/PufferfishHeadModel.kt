c5d748ad06537a83eaf7a027a97396926bc0ed8f
TheEnderCore <theendercore@gmail.com>
2025-09-24 15:43:19 +0300

    PufferfishHeadModel

--- src/client/kotlin/org/teamvoided/all_the_heads/client/model/PufferfishHeadModel.kt
+++ src/client/kotlin/org/teamvoided/all_the_heads/client/model/PufferfishHeadModel.kt
@@ -8,16 +8,22 @@ import net.minecraft.client.model.geom.builders.CubeListBuilder
 import net.minecraft.client.model.geom.builders.LayerDefinition
 import net.minecraft.client.model.geom.builders.MeshDefinition
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.util.Mth
 import org.teamvoided.all_the_heads.AllTheHeads
 import org.teamvoided.all_the_heads.client.model.utils.HeadModelBase
 
 class PufferfishHeadModel(modelPart: ModelPart) : HeadModelBase() {
     override fun getId(): ResourceLocation = ID
     private val head: ModelPart = modelPart.getChild("head")
+    private val leftBlueFin: ModelPart = head.getChild("left_blue_fin")
+    private val rightBlueFin: ModelPart = head.getChild("right_blue_fin")
 
-    override fun setupAnim(f: Float, g: Float, h: Float) {
+    override fun setupAnim(animTick: Float, g: Float, h: Float) {
         head.yRot = g * (Math.PI / 180.0).toFloat()
         head.xRot = h * (Math.PI / 180.0).toFloat()
+
+        leftBlueFin.zRot = -0.2f + 0.4f * Mth.sin(animTick * 0.2f)
+        rightBlueFin.zRot = 0.2f - 0.4f * Mth.sin(animTick * 0.2f)
     }
 
     override fun renderToBuffer(poseStack: PoseStack, vertexConsumer: VertexConsumer, i: Int, j: Int, k: Int) {
@@ -28,9 +34,69 @@ class PufferfishHeadModel(modelPart: ModelPart) : HeadModelBase() {
         val ID = AllTheHeads.id("pufferfish")
         fun head(): LayerDefinition {
             val mesh = MeshDefinition()
-            mesh.root.addOrReplaceChild(
-                "head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F),
+            val head = mesh.root.addOrReplaceChild(
+                "head", CubeListBuilder.create().texOffs(0, 0).addBox(-4f, -8f, -4f, 8f, 8f, 8f),
                 PartPose.ZERO
+            )
+            head.addOrReplaceChild(
+                "right_blue_fin",
+                CubeListBuilder.create().texOffs(24, 0).addBox(-2f, 0f, -1f, 2f, 1f, 2f),
+                PartPose.offset(-4f, -7f, -2f)
+            )
+            head.addOrReplaceChild(
+                "left_blue_fin",
+                CubeListBuilder.create().texOffs(24, 3).addBox(0f, 0f, -1f, 2f, 1f, 2f),
+                PartPose.offset(4f, -7f, -2f)
+            )
+            head.addOrReplaceChild(
+                "top_front_fin",
+                CubeListBuilder.create().texOffs(15, 17).addBox(-4f, -1f, 0f, 8f, 1f, 0f),
+                PartPose.offsetAndRotation(0f, -8f, -4f, (Math.PI / 4).toFloat(), 0f, 0f)
+            )
+            head.addOrReplaceChild(
+                "top_middle_fin",
+                CubeListBuilder.create().texOffs(14, 16).addBox(-4f, -1f, 0f, 8f, 1f, 1f),
+                PartPose.offset(0f, -8f, 0f)
+            )
+            head.addOrReplaceChild(
+                "top_back_fin",
+                CubeListBuilder.create().texOffs(23, 18).addBox(-4f, -1f, 0f, 8f, 1f, 0f),
+                PartPose.offsetAndRotation(0f, -8f, 4f, (-Math.PI / 4).toFloat(), 0f, 0f)
+            )
+            head.addOrReplaceChild(
+                "right_front_fin",
+                CubeListBuilder.create().texOffs(5, 17).addBox(-1f, -8f, 0f, 1f, 8f, 0f),
+                PartPose.offsetAndRotation(-4f, 0f, -4f, 0f, (-Math.PI / 4).toFloat(), 0f)
+            )
+            head.addOrReplaceChild(
+                "left_front_fin",
+                CubeListBuilder.create().texOffs(1, 17).addBox(0f, -8f, 0f, 1f, 8f, 0f),
+                PartPose.offsetAndRotation(4f, 0f, -4f, 0f, (Math.PI / 4).toFloat(), 0f)
+            )
+            head.addOrReplaceChild(
+                "bottom_front_fin",
+                CubeListBuilder.create().texOffs(15, 20).addBox(-4f, 0f, 0f, 8f, 1f, 0f),
+                PartPose.offsetAndRotation(0f, 0f, -4f, (-Math.PI / 4).toFloat(), 0f, 0f)
+            )
+            head.addOrReplaceChild(
+                "bottom_middle_fin",
+                CubeListBuilder.create().texOffs(15, 20).addBox(-4f, 0f, 0f, 8f, 1f, 0f),
+                PartPose.offset(0f, 0f, 0f)
+            )
+            head.addOrReplaceChild(
+                "bottom_back_fin",
+                CubeListBuilder.create().texOffs(15, 20).addBox(-4f, 0f, 0f, 8f, 1f, 0f),
+                PartPose.offsetAndRotation(0f, 0f, 4f, (Math.PI / 4).toFloat(), 0f, 0f)
+            )
+            head.addOrReplaceChild(
+                "right_back_fin",
+                CubeListBuilder.create().texOffs(9, 17).addBox(-1f, -8f, 0f, 1f, 8f, 0f),
+                PartPose.offsetAndRotation(-4f, 0f, 4f, 0f, (Math.PI / 4).toFloat(), 0f)
+            )
+            head.addOrReplaceChild(
+                "left_back_fin",
+                CubeListBuilder.create().texOffs(9, 17).addBox(0f, -8f, 0f, 1f, 8f, 0f),
+                PartPose.offsetAndRotation(4f, 0f, 4f, 0f, (-Math.PI / 4).toFloat(), 0f)
             )
             return LayerDefinition.create(mesh, 32, 32)
         }
