1af606249ab29eea2abc70359adc16835409078f
TheEnderCore <theendercore@gmail.com>
2025-09-25 14:23:30 +0300

    added hat for VillagerHeadModel.

--- src/client/kotlin/org/teamvoided/all_the_heads/client/model/VillagerHeadModel.kt
+++ src/client/kotlin/org/teamvoided/all_the_heads/client/model/VillagerHeadModel.kt
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.blaze3d.vertex.VertexConsumer
 import net.minecraft.client.model.geom.ModelPart
 import net.minecraft.client.model.geom.PartPose
+import net.minecraft.client.model.geom.builders.CubeDeformation
 import net.minecraft.client.model.geom.builders.CubeListBuilder
 import net.minecraft.client.model.geom.builders.LayerDefinition
 import net.minecraft.client.model.geom.builders.MeshDefinition
@@ -37,6 +38,17 @@ class VillagerHeadModel(modelPart: ModelPart) : HeadModelBase() {
                 "nose",
                 CubeListBuilder.create().texOffs(24, 0).addBox(-1f, -1f, -6f, 2f, 4f, 2f),
                 PartPose.offset(0f, -2f, 0f)
+            )
+            val hat = head.addOrReplaceChild(
+                "hat",
+                CubeListBuilder.create().texOffs(32, 0)
+                    .addBox(-4.0f, -10.0f, -4.0f, 8.0f, 10.0f, 8.0f, CubeDeformation(0.5f)),
+                PartPose.ZERO
+            )
+            hat.addOrReplaceChild(
+                "hat_rim",
+                CubeListBuilder.create().texOffs(30, 47).addBox(-8.0f, -8.0f, -6.0f, 16.0f, 16.0f, 1.0f),
+                PartPose.rotation((-Math.PI / 2).toFloat(), 0.0f, 0.0f)
             )
             return LayerDefinition.create(mesh, 64, 64)
         }
