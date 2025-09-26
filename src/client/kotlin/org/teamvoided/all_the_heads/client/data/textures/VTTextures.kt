2aac761205b588854ef7405d55fd3f20ad573687
TheEnderCore <theendercore@gmail.com>
2025-09-26 15:12:15 +0300

    SpellCheckingInspection for textures

--- src/client/kotlin/org/teamvoided/all_the_heads/client/data/textures/VTTextures.kt
+++ src/client/kotlin/org/teamvoided/all_the_heads/client/data/textures/VTTextures.kt
@@ -1,5 +1,6 @@
 package org.teamvoided.all_the_heads.client.data.textures
 
+@Suppress("SpellCheckingInspection")
 object VTTextures {
     const val ALLAY =
         "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvY2MwMzg5MTc3ZGJhYTkyZjBkNWZmZGY4NDg4NjJjN2Y5YjM2ZGYyMjJmYmZkNzM3ZTI2MzlkYzMwNTllMGNmMyJ9fX0="
